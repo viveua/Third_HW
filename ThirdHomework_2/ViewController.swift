c555ed03accd333acf0d6505c2bd0488d861ed01
Mac <mac@Macs-iMac.local>
2017-06-24 19:08:05 +0300

    Initial Commit

---
+++ ThirdHomework_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThirdHomework_2
+//
+//  Created by Mac on 24.06.17.
+//  Copyright © 2017 Web Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
